c461df4c338b9b29d6b8db3a65d8617b350d5a7b
TUSHAR <125198098+tusharagrahari214@users.noreply.github.com>
2026-07-10 23:25:28 +0530

    Time: 714 ms (39.86%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+
+
+SELECT s.user_id,
+       ROUND(
+            IFNULL(
+                    AVG(
+                        CASE 
+                            WHEN c.action = 'timeout' THEN 0 
+                            WHEN c.action IS NULL THEN 0
+                            ELSE 1 
+                        END
+                    ),
+                    0
+                ),
+                2) AS confirmation_rate
+       
+FROM Signups AS s
+LEFT JOIN Confirmations AS c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
+
