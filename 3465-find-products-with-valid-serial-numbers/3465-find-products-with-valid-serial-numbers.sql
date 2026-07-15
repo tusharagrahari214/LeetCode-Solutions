d8925907e923b8f0b2b5a8f7680eafe613245579
TUSHAR <125198098+tusharagrahari214@users.noreply.github.com>
2026-07-15 18:32:37 +0530

    Time: 555 ms (14.09%), Space: 0B (100%) - LeetHub

---
+++ 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+
+SELECT
+    product_id,
+    product_name,
+    description
+FROM products
+
+WHERE description REGEXP '(?-i)\\bSN[0-9]{4}-[0-9]{4}\\b'
+ORDER BY product_id;
