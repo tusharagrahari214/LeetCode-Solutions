4bf816c37f94f1974ea403a0bad97c8db7a346ad
TUSHAR <125198098+tusharagrahari214@users.noreply.github.com>
2026-08-03 23:54:36 +0530

    Time: 438 ms (10.36%), Space: 0B (100%) - LeetHub

---
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -0,0 +1,31 @@
+# Write your MySQL query statement below
+
+SELECT customer_id
+FROM customer_transactions
+GROUP BY customer_id
+HAVING
+    SUM(
+        CASE
+            WHEN transaction_type='purchase'
+            THEN 1
+            ELSE 0
+        END
+    ) >= 3
+
+    AND
+
+    DATEDIFF(
+        MAX(transaction_date),
+        MIN(transaction_date)
+    ) >= 30
+
+    AND
+
+    SUM(
+        CASE
+            WHEN transaction_type='refund'
+            THEN 1
+            ELSE 0
+        END
+    ) / COUNT(*) < 0.2
+ORDER BY customer_id;
